a136da723a8317d40711d41825bb58a3f2501260
Bedelo <s_ceb@hotmail.com>
2021-02-01 20:37:42 +0100

    ajout de joueurs.json , modification de index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
+const e = require('express')
 const express = require('express')
 const app = express()
 
 const equipes = require('./equipes.json')
+const joueurs = require('./joueurs.json')
 
 //Middleware
 app.use(express.json())
@@ -10,6 +13,8 @@ app.listen('5000', () => {
     console.log('rest api via expressjs')
 })
 
+
+// EQUIPES PATHS FROM .json
 app.get('/equipes' , (req, res) => {
     //res.send('liste des equipes');
     res.status(200).json(equipes)
@@ -34,9 +39,31 @@ app.put('/equipes/:id' , (req, res) => {
     req.status(200).json(equipes)
 })
 
-app.delete('equipe/:id', (req, res) => {
+app.delete('/equipe/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const equipe = equipes.find( equipe => equipes.id === id)
     equipes.splice(equipes.indexOf(equipe), 1)
-    res.status(200).json(equuipes)
+    res.status(200).json(equipes)
+})
+
+// JOUEURS PATHS FROM .json
+app.get('/joueurs', (req, res) => {
+    res.status(200).json(joueurs)
+})
+
+app.get('/joueur/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const joueur = joueurs.find(joueur => joueur.id === id)
+    res.status(200).json(joueur)
+})
+
+//JOUEURS from equipe.json
+app.get('/joueursOf/:idEquipe' , (req, res) => {
+    const idEquipe = parseInt(req.params.idEquipe)
+    let joueursOfTeam = [];
+    joueurs.map((j)=>{
+        if(j.idEquipe === idEquipe)
+            joueursOfTeam.push(j)
+    })
+    res.status(200).json(joueursOfTeam)
 })
